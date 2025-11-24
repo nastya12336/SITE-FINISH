10e63c20fa08f08b1615051d1e89cfc57be0999c
nastya12336 <nastyaguseva09@mail.ru>
2025-11-24 16:15:22 +0300

    Add files via upload

---
+++ movies-videos.js
@@ -0,0 +1,55 @@
+// movies-videos.js
+const videoDatabase = {
+    // Комедии
+    1: "https://example.com/films/1.mp4",  // Мальчишник в Вегасе
+    2: "https://example.com/films/2.mp4",  // Одноклассники
+    3: "https://example.com/films/3.mp4",  // Красотка на всю голову
+    4: "https://example.com/films/4.mp4",  // Мачо и ботан
+    5: "https://example.com/films/5.mp4",  // Третий лишний
+    
+    // Драмы
+    19: "https://example.com/films/19.mp4", // Зеленая миля
+    20: "https://example.com/films/20.mp4", // Побег из Шоушенка
+    21: "https://example.com/films/21.mp4", // Форрест Гамп
+    22: "https://example.com/films/22.mp4", // Леон
+    23: "https://example.com/films/23.mp4", // Зеленая книга
+    
+    // Триллеры
+    32: "https://example.com/films/32.mp4", // Исчезнувшая
+    33: "https://example.com/films/33.mp4", // Молчание ягнят
+    34: "https://example.com/films/34.mp4", // Игра
+    
+    // Боевики
+    40: "https://example.com/films/40.mp4", // Безумный Макс
+    42: "https://example.com/films/42.mp4", // Крепкий орешек
+    43: "https://example.com/films/43.mp4", // Убить Билла
+    
+    // Фэнтези
+    48: "https://example.com/films/48.mp4", // Властелин колец
+    49: "https://example.com/films/49.mp4", // Бессмертная гвардия
+    
+    // Фантастика
+    52: "https://example.com/films/52.mp4", // Интерстеллар
+    54: "https://example.com/films/54.mp4", // Матрица
+    55: "https://example.com/films/55.mp4", // Бегущий по лезвию
+    
+    // Мелодрамы
+    61: "https://example.com/films/61.mp4", // Титаник
+    62: "https://example.com/films/62.mp4", // Гордость и предубеждение
+    
+    // Ужасы
+    71: "https://example.com/films/71.mp4", // Сияние
+    72: "https://example.com/films/72.mp4", // Чужой
+    
+    // Приключения
+    81: "https://example.com/films/81.mp4", // Индиана Джонс
+    82: "https://example.com/films/82.mp4", // Пираты Карибского моря
+    
+    // Детективы
+    100: "https://example.com/films/100.mp4", // Семь
+    101: "https://example.com/films/101.mp4", // Достать ножи
+    
+    // Анимация
+    200: "https://example.com/films/200.mp4", // Король Лев
+    201: "https://example.com/films/201.mp4", // Тайна Коко
+};
